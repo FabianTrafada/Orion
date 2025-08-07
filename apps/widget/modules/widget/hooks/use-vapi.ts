36c765aaa4d0fbb644bcb5b0cdad92d6d4b0f2a7
FabianPerkasa <fabiantrafadaperkasa@gmail.com>
2025-08-08 00:00:48 +0700

    feat: implement Vapi integration in widget app for voice interaction

---
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -0,0 +1,86 @@
+import Vapi from "@vapi-ai/web";
+import { useEffect, useState } from "react";
+
+interface TranscriptMessage {
+    role: "user" | "assistant";
+    text: string;
+}
+
+export const useVapi = () => {
+    const [vapi, setVapi] = useState<Vapi | null>(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [isConnecting, setIsConnecting] = useState(false);
+    const [isSpeaking, setIsSpeaking] = useState(false);
+    const [transcript, setTranscript] = useState<TranscriptMessage[]>([]);
+
+    useEffect(() => {
+        // Only for testing Vapi API, otherwise the users must provider their own API public keys
+        const vapiInstance = new Vapi("d8c12dad-b2ec-4505-bc44-049e4553c443")
+        setVapi(vapiInstance);
+
+        vapiInstance.on("call-start", () => {
+            setIsConnected(true);
+            setIsConnecting(false);
+            setTranscript([]);
+        });
+
+        vapiInstance.on("call-end", () => {
+            setIsConnected(false);
+            setIsConnecting(false);
+            setIsSpeaking(false);
+        });
+
+        vapiInstance.on("speech-start", () => {
+            setIsSpeaking(true);
+        });
+
+        vapiInstance.on("speech-end", () => {
+            setIsSpeaking(false);
+        });
+
+        vapiInstance.on("error", (error) => {
+            console.log("Vapi error", error);
+            setIsConnecting(false);
+        });
+
+        vapiInstance.on("message", (message) => {
+            if (message.type === "transcript" && message.transcriptType === "final") {
+                setTranscript((prev) => [
+                    ...prev,
+                    {
+                        role: message.role === "user" ? "user" : "assistant",
+                        text: message.transcript,
+                    }
+                ]);
+            }
+        });
+
+        return () => {
+            vapiInstance?.stop();
+        }
+    }, []);
+
+    const startCall = () => {
+        setIsConnecting(true);
+
+        if (vapi) {
+            // Only for testing Vapi API, otherwise the users must provider their own assistant Id
+            vapi.start("588c4310-3849-4126-9b85-5daea6d506d3");
+        }
+    }
+
+    const endCall = () => {
+        if (vapi) {
+            vapi.stop();
+        }
+    }
+
+    return {
+        isSpeaking,
+        isConnecting,
+        isConnected,
+        transcript,
+        startCall,
+        endCall
+    }
+}
