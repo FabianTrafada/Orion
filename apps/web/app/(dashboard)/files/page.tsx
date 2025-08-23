76accd1cb4b993a0a58ef361c821f801b544700f
FabianPerkasa <fabiantrafadaperkasa@gmail.com>
2025-08-24 00:58:24 +0700

    feat: implement file upload and delete dialogs, enhance file management UI with dropzone support

--- apps/web/app/(dashboard)/files/page.tsx
+++ apps/web/app/(dashboard)/files/page.tsx
@@ -1,9 +1,7 @@
+import { FilesView } from "@/modules/files/ui/views/files-view";
+
 const Page = () => {
-    return (
-        <div>
-            Files Page
-        </div>
-    )
+    return <FilesView />
 }
 
 export default Page;
